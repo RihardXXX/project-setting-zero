7b8ef7ff1467540fb03f465806bedcdb0d5044be
RihardXXX <yusupa.akaeva@yandex.ru>
2021-09-01 20:37:02 +0300

    connect preset plugins and proposal

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
-const run = () => console.log('run');
+const run = (a) => console.log(`un $${a}`);
 
-run();
+class Foo {
+  constructor(test) {
+    this.test = test;
+  }
+}
+
+const arrowFunc = (a) => a * a;
+
+const arr = [1, 2, 3, 4, 5];
+const spreadOper = [...arr];
+
+run(5);
+
+// plugin-proposal-optional-chaining
+const obj = {
+  foo: {
+    bar: {
+      baz: 42,
+    },
+  },
+};
+
+const baz = obj?.foo?.bar?.baz; // 42
+
+// plugin-proposal-function-sent
+function* generator() {
+  console.log('Sent', yield);
+  console.log('Yield', yield);
+}
+
+const iterator = generator();
+iterator.next(1); // Logs "Sent 1"
+iterator.next(2); // Logs "Yield 2"
